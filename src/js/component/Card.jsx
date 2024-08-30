51461dc207aed24ca878645ae92676b07a8e6ad4
Bradley Aikins <126637367+IssaPanda@users.noreply.github.com>
2024-08-30 20:16:19 +0000

    First commit

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({title, text}) => {
+    return(
+        <div class="card" style={{ width: '18rem' }}>
+  <img src="https://qph.cf2.quoracdn.net/main-qimg-bc54d4d995526ae5eb9877c1c00ce725-lq" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{text}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    )
+};
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
 import React from "react";
+import { createRoot } from 'react-dom/client';
+import Navbar from "../../js/component/Navbar.jsx";
+import Jumbotron from "../../js/component/Jumbotron.jsx";
+import Card from "../../js/component/Card.jsx";
+
+const domNode = document.getElementById('app');
+const root = createRoot(domNode);
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const cardContent = [
+		{
+            title: "Card 1",
+            text: "This is the first card's content."
+        },
+        {
+            title: "Card 2",
+            text: "This is the second card's content."
+        },
+        {
+            title: "Card 3",
+            text: "This is the third card's content."
+        },
+        {
+            title: "Card 4",
+            text: "This is the fourth card's content."
+        }
+	]
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<><Navbar /><div className="container">
+			<Jumbotron />
+			<div className="row">
+                {cardContent.map((card, index) => (
+                    <div className="col-md-3" key={index}>
+                        <Card
+                            title={card.title}
+                            text={card.text}
+                        />
+                    </div>
+                ))}
+            </div>
+		</div></>
 	);
 };
 
 export default Home;
+
