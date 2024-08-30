4e78d56490554fd3cd1ceb9a6b63f9cc9018791e
Bradley Aikins <126637367+IssaPanda@users.noreply.github.com>
2024-08-30 20:36:16 +0000

    Second commit

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = () => {
     return(
-        <div class="jumbotron bg-light">
+        <div class="jumbotron bg-light p-5">
         <h1 class="display-4">Hello, world!</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr class="my-4"/>
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Navbar = () => {
     return(
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+  <div class="container-fluid bg-secondary">
+    <a class="navbar-brand text-white" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+        <a class="nav-link text-white" href="#">Features</a>
+        <a class="nav-link text-white" href="#">Pricing</a>
+        <a class="nav-link disabled text-white" aria-disabled="true">Disabled</a>
       </div>
     </div>
   </div>
